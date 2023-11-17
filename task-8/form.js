efc5c40d330ff3769fe3bdc7869f6d4462c8b75e
ChursinAlexUnder <144013544+ChursinAlexUnder@users.noreply.github.com>
2023-11-18 00:33:16 +0300

    Add files via upload

--- task-8/form.js
+++ task-8/form.js
@@ -1,49 +1,51 @@
-document.getElementById("openFormButton").addEventListener("click", function() {
-    document.getElementById("popup").style.display = "block";
-    history.pushState(null, null, "#popup");
-});
-
-document.querySelector(".close").addEventListener("click", function() {
-    document.getElementById("popup").style.display = "none";
-    history.pushState(null, null, " ");
-});
-
-window.addEventListener("popstate", function(event) {
-    if (event.state !== null) {
-        document.getElementById("popup").style.display = "block";
-    } else {
-        document.getElementById("popup").style.display = "none";
-    }
-});
-
-document.getElementById("feedbackForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    var form = event.target;
-    var formData = new FormData(form);
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", form.action, true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            // Отправка успешна
-            alert("Сообщение успешно отправлено!");
-            form.reset();
-        } else if (xhr.readyState === 4 && xhr.status !== 200) {
-            // Ошибка отправки
-            alert("Произошла ошибка при отправке сообщения. Пожалуйста, попробуйте еще раз.");
-        }
-    };
-    xhr.send(formData);
-});
-
-// Восстановление последних введенных значений из LocalStorage
-var inputs = document.querySelectorAll("input, textarea");
-for (var i = 0; i < inputs.length; i++) {
-    var input = inputs[i];
-    var value = localStorage.getItem(input.id);
-    if (value) {
-        input.value = value;
-    }
-    input.addEventListener("input", function(event) {
-        localStorage.setItem(event.target.id, event.target.value);
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("openFormButton").addEventListener("click", function() {
+      document.getElementById("popup").style.display = "block";
+      history.pushState(null, null, "#popup");
     });
-}
+  
+    document.querySelector(".close").addEventListener("click", function() {
+      document.getElementById("popup").style.display = "none";
+      history.pushState(null, null, " ");
+    });
+  
+    window.addEventListener("popstate", function(event) {
+      if (event.state !== null) {
+        document.getElementById("popup").style.display = "block";
+      } else {
+        document.getElementById("popup").style.display = "none";
+      }
+    });
+  
+    document.getElementById("feedbackForm").addEventListener("submit", function(event) {
+      event.preventDefault();
+      var form = event.target;
+      var formData = new FormData(form);
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", form.action, true);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          // Отправка успешна
+          alert("Сообщение успешно отправлено!");
+          form.reset();
+        } else if (xhr.readyState === 4 && xhr.status === 500) {
+          // Ошибка отправки
+          alert("Произошла ошибка при отправке сообщения. Пожалуйста, попробуйте еще раз.");
+        }
+      };
+      xhr.send(formData);
+    });
+  
+    // Восстановление последних введенных значений из LocalStorage
+    var inputs = document.querySelectorAll("input, textarea");
+    for (var i = 0; i < inputs.length; i++) {
+      var input = inputs[i];
+      var value = localStorage.getItem(input.id);
+      if (value) {
+        input.value = value;
+      }
+      input.addEventListener("input", function(event) {
+        localStorage.setItem(event.target.id, event.target.value);
+      });
+    }
+  });
